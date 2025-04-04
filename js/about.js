745eca8b1bd130353e8184e4a78b44827e8fb18f
Augustinas Gad <augustinas.gadliauskas@stud.techin.lt>
2025-04-04 14:02:24 +0300

    BIG UPDATE - very much content

---
+++ js/about.js
@@ -0,0 +1,6 @@
+const oval=document.getElementsByClassName("circle-inside")
+const div=document.getElementsByClassName("div1")
+
+oval.addEventListener("click", ()=>{
+    oval.style.fill='green'
+})
