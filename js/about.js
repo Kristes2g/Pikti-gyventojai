5784f45046ee81a55f9901d787b518fb25a28009
Augustinas Gad <augustinas.gadliauskas@stud.techin.lt>
2025-04-14 14:04:07 +0300

    korteles juda ir spalvos grazios

--- js/about.js
+++ js/about.js
@@ -6,3 +6,11 @@ var buttons = document.querySelectorAll('.rotate, .fill');
   });
 });
     
+
+var cards = document.querySelectorAll('.card');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
