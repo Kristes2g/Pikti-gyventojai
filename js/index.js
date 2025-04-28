09fc2194701c19d942a333533e4759d72e2df1a9
Oskaras Vol <oskaras.volodkovicius@stud.techin.lt>
2025-04-28 09:13:53 +0300

    beveik uzbaigtas hamburger

--- js/index.js
+++ js/index.js
@@ -2,7 +2,12 @@ const hamburger = document.querySelector(".hamburger")
 const menu = document.querySelector(".NavList")
 const kryziukas = document.querySelector(".kryziukas")
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', ()=>{
     menu.classList.toggle('active') 
     kryziukas.classList.toggle('active')
 });
+
+kryziukas.addEventListener('click', ()=>{
+    menu.classList.toggle('active')
+    kryziukas.classList.toggle('active')
+})
